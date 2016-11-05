cddb3cd6b3563cae83a92f9c04bcd32f889fe303
tksmaru <tksmaru@gmail.com>
2016-11-06 00:53:29 +0900

    devide logics and html.

--- contents/oneline_bbs/bbs.php
+++ contents/oneline_bbs/bbs.php
@@ -77,24 +77,29 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // 投稿された内容を取得するSQLを作成して結果を取得
     $sql = "SELECT * FROM `post` ORDER BY `created_at` DESC";
     $result = mysql_query($sql, $link);
+
+    $posts = array();
+    if ($result !== false && mysql_num_rows($result)) {
+      while ($post = mysql_fetch_assoc($result)) {
+        $posts[] = $post;
+      }
+    }
+    // 取得結果を解放して接続を閉じる
+    mysql_free_result($result);
+    mysql_close($link);
   ?>
-  <?php if ($result !== false && mysql_num_rows($result)): ?>
+
+  <?php if (count($posts) > 0): ?>
   <ul>
-    <?php while ($post = mysql_fetch_assoc($result)): ?>
+    <?php foreach ($posts as $post): ?>
     <li>
       <?php echo htmlspecialchars($post['name'], ENT_QUOTES, 'UTF-8'); ?>:
       <?php echo htmlspecialchars($post['comment'], ENT_QUOTES, 'UTF-8'); ?>
       - <?php echo htmlspecialchars($post['created_at'], ENT_QUOTES, 'UTF-8'); ?>
     </li>
-    <?php endwhile; ?>
+  <?php endforeach; ?>
   </ul>
   <?php endif; ?>
-
-  <?php
-    // 取得結果を解放して接続を閉じる
-    mysql_free_result($result);
-    mysql_close($link);
-  ?>
 
 </body>
 </html>
