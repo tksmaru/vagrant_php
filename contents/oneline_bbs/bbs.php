a40a7d685d1fb788295f0929ebfc79efc1258cf0
tksmaru <tksmaru@gmail.com>
2016-11-06 01:14:54 +0900

    devided logic and view to other file.

--- contents/oneline_bbs/bbs.php
+++ contents/oneline_bbs/bbs.php
@@ -48,58 +48,19 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   }
 }
 
-?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-  "http://www.w3.org/TR/xhtml/1/DTD/xhtml1-transitional.dtd">
-<html>
-  <head>
-    <title>ひとこと掲示板</title>
-  </head>
-<body>
-  <h1>ひとこと掲示板</h1>
-  <form action="bbs.php" method="post">
-    <?php if (count($errors)): ?>
-    <ul class="error_list">
-      <?php foreach ($errors as $error): ?>
-      <li>
-        <?php echo htmlspecialchars($error, ENT_QUOTES, 'UTF-8') ?>
-      </li>
-      <?php endforeach; ?>
-    </ul>
-    <?php endif; ?>
-    名前: <input type="text" name="name" /><br />
-    ひとこと: <input type="text" name="comment" size="60" /><br />
-    <input type="submit" name="submit" value="送信" />
-  </form>
+// 投稿された内容を取得するSQLを作成して結果を取得
+$sql = "SELECT * FROM `post` ORDER BY `created_at` DESC";
+$result = mysql_query($sql, $link);
 
-  <?php
-    // 投稿された内容を取得するSQLを作成して結果を取得
-    $sql = "SELECT * FROM `post` ORDER BY `created_at` DESC";
-    $result = mysql_query($sql, $link);
+$posts = array();
+if ($result !== false && mysql_num_rows($result)) {
+  while ($post = mysql_fetch_assoc($result)) {
+    $posts[] = $post;
+  }
+}
+// 取得結果を解放して接続を閉じる
+mysql_free_result($result);
+mysql_close($link);
 
-    $posts = array();
-    if ($result !== false && mysql_num_rows($result)) {
-      while ($post = mysql_fetch_assoc($result)) {
-        $posts[] = $post;
-      }
-    }
-    // 取得結果を解放して接続を閉じる
-    mysql_free_result($result);
-    mysql_close($link);
-  ?>
-
-  <?php if (count($posts) > 0): ?>
-  <ul>
-    <?php foreach ($posts as $post): ?>
-    <li>
-      <?php echo htmlspecialchars($post['name'], ENT_QUOTES, 'UTF-8'); ?>:
-      <?php echo htmlspecialchars($post['comment'], ENT_QUOTES, 'UTF-8'); ?>
-      - <?php echo htmlspecialchars($post['created_at'], ENT_QUOTES, 'UTF-8'); ?>
-    </li>
-  <?php endforeach; ?>
-  </ul>
-  <?php endif; ?>
-
-</body>
-</html>
+include 'views/bbs_view.php';
